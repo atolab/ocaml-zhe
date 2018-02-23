2a4dc6ca15ff0f5cb49f7d1448e0787365302d2e
Angelo Corsaro <angelo@icorsaro.net>
2018-02-23 23:42:54 +0100

    initial version of ocaml API

--- bin/pub.ml
+++ bin/pub.ml
@@ -7,13 +7,14 @@ let port =  7447
 let () =
   let z = zhe(port) in
   let p = zhe_publish 1 0 1 in
+  let period = 1000000000 in
   let rec loop n =
     match n with
     | i when i > 0 ->
       let t = zhe_platform_time () in
       ignore (zhe_write p "hello" 5 t ) ;
-      zhe_flush ();
-      zhe_once z 1000000000 ;
+      zhe_flush () ;
+      zhe_once z period ;
       loop (n - 1)
     | _ -> ()
   in
